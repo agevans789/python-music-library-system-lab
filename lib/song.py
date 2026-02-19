4da40094234a61f9c04d6a244284415a6ee67cb2
Ann Grace Evans <agevans789@gmail.com>
2026-02-19 12:05:44 -0500

    1 passed

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,28 @@
 class Song:
-    pass
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artists_count = {}
+
+    def add_song_to_count(self):
+        Song.count += 1
+
+    def add_to_genres(self):
+        if self.genre not in Song.genres:
+            Song.genres.append(self.genre)
+
+    def add_to_artists(self):
+        if self.artist not in Song.artists:
+            Song.artists.append(self.artist)
+
+    def add_to_genre_count(self):
+        Song.genre_count[self.genre] = Song.genre_count.get(self.genre, 0) + 1
+
+    def add_to_artist_count(self):
+        Song.artists_count[self.artist] = Song.artists_count.get(self.artist, 0) + 1
